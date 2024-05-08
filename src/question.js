e6d3235a56c655102f93e761cefb7f64a714e3bb
Ashok Kumar D <goldenashok@users.noreply.github.com>
2024-05-08 21:34:34 +0530

    Update question.js

---
+++ src/question.js
@@ -0,0 +1,39 @@
+export const questions = [
+    {
+        id: 1,
+        q: 'Whos is the father of the computer?',
+        o1: 'Issac Newton',
+        o2: 'Charlas Babbage',
+        o3: 'Marie Cuire'
+    },
+    {
+        id: 2,
+        q: 'Whos is the father of the India?',
+        o1: 'Mahatma Gandhi',
+        o2: 'Nethaji',
+        o3: 'Nehru'
+    },
+    {
+        id: 3,
+        q: 'What is the capital of the India?',
+        o1: 'Chennai',
+        o2: 'Bombay',
+        o3: 'Delhi'
+    },
+    {
+        id: 4,
+        q: 'Who is the prime minister of india?',
+        o1: 'Mamtha banerji',
+        o2: 'Narendra modi',
+        o3: 'Edapadi Palanisamy'
+    },
+    {
+        id: 5,
+        q: 'Who wrote national anthan for india?',
+        o1: 'Rabindranath Tagore',
+        o2: 'Bharathiyar',
+        o3: 'Thiruvalluvar'
+    }
+]
+
+export const answer = ['Charlas Babbage', 'Mahatma Gandhi', 'Delhi', 'Narendra modi', 'Rabindranath Tagore'];
